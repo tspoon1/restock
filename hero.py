caeea9f173e073dfaa925f43dcb5cd925dddb77b
Tim <tspalmieri2018@gmail.com>
2020-05-05 22:07:34 -0400

    setting up heroku

---
+++ hero.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def isInStock(product_url, chromeDriverPath):
+
+    inStock = False
+
+    #  IN "HEADLESS MODE  "
+    #LOGGER.setLevel(logging.WARNING)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
+
+    driver.get(product_url)
+
+    try:
+        price = driver.find_element_by_id("priceblock_ourprice").text
+        if "$" in price:
+            inStock = True
+    except Exception as e:
+        pass
+
+    driver.quit()
+
+    return inStock
+
+def initSheet():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
+    client = gspread.authorize(creds)
+    sheet = client.open("Restock").sheet1
+
+    return sheet
+
+def addNewRow(row):
+    sheet.insert_row(row, 2)
+
+
+url = "https://www.amazon.com/Nintendo-Switch-Neon-Blue-Joy%E2%80%91/dp/B07VGRJDFY/ref=sr_1_3?crid=IY3K5B823UOZ&dchild=1&keywords=nintendo+switch&qid=1588716182&sprefix=nintend%2Caps%2C159&sr=8-3"
+CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+sheet = initSheet()
+
+#print(isInStock(url, CHROMEDRIVER_PATH))
+email = ""
+
+if isInStock(url, CHROMEDRIVER_PATH):
+    print("Oh, your item is in stock. Maybe try a different link!")
+else:
+    print("Looks like your item is indeed out of stock.")
+    print("If you would like us to send you an email when")
+    print("your item is back in stock, enter it below!")
+    email = input("-->")
+    
+    while ("@" not in email):
+        print("Whoops, please enter a valid email.")
+        email = input("-->")
+
+    print("Adding your request to our database...")
+    newRow = [email, url]
+    
+    addNewRow(newRow)
+
+    print("Thanks for adding your item to Restock!\n")
---
+++ restock_heroku.py
@@ -0,0 +1,99 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import os
+
+load_dotenv()
+
+def isInStock(product_url, chromeDriverPath):
+
+    inStock = False
+
+    #  IN "HEADLESS MODE  "
+    options = webdriver.ChromeOptions()
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
+
+    driver.get(product_url)
+
+    try:
+        price = driver.find_element_by_id("priceblock_ourprice").text
+        if "$" in price:
+            inStock = True
+    except Exception as e:
+        pass
+
+    driver.quit()
+
+    return inStock
+
+def initSheet():
+
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
+    client = gspread.authorize(creds)
+    sheet = client.open("Restock").sheet1
+
+    return sheet
+
+def notifyUser(email, url):
+
+    SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+    SENDGRID_TEMPLATE_ID = os.environ.get("SENDGRID_TEMPLATE_ID")
+    MY_ADDRESS = os.environ.get("EMAIL")
+    SUBJECT = 'Restock.io: Updates on the availability and price of your product(s)'
+
+    client = SendGridAPIClient(SENDGRID_API_KEY)
+    print("CLIENT:", type(client))
+
+    message = Mail(from_email=MY_ADDRESS, to_emails=email, subject=SUBJECT)
+    print("MESSAGE:", type(message))
+
+    message.template_id = SENDGRID_TEMPLATE_ID
+
+    message.dynamic_template_data = {
+        "name": customer_name
+        "human_friendly_timestamp": now.strftime("%d-%m-%Y %I:%M %p"),
+        "products": products_list
+        }
+
+    try:
+        response = client.send(message)
+        print("RESPONSE:", type(response))
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+
+    except Exception as e:
+        print("OOPS", e)
+        print("--------ERROR SENDING---------")
+        print(f"The row deleted but that didn't send correctly was {email}")
+        print(url)
+        print("--------ERROR SENDING---------")
+
+
+if __name__ == "__main__":
+
+    CHROMEDRIVER_PATH = "c:/Users/timpa/Documents/GitHub/restock/chromedriver.exe"
+    #NEED TO FIGURE OUT WHAT THE PATH TO THE HEROKU DIRECTORY IS!
+
+    sheet = initSheet()
+    email = "temp@email.com"
+
+    emailList = sheet.col_values(1)[1:]
+    urlList = sheet.col_values(2)[1:]
+    rowNumber = 2
+
+    for email, url in zip(emailList, urlList):
+        if isInStock(url, CHROMEDRIVER_PATH):
+            notifyUser(email, url)
+            sheet.delete_row(rowNumber)
+        rowNumber += 1
